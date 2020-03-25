2f4c4befceff5a78dcce09d2fd97cdb268939333
excelwang <wanghuajin999@163.com>
2020-03-25 11:06:06 +0800

    add queryparser dependency and search test case

--- node/src/test/scala/IndexSearchCase.scala
+++ node/src/test/scala/IndexSearchCase.scala
@@ -5,11 +5,17 @@ import org.apache.lucene.document.Document
 import org.apache.lucene.document.Field
 import org.apache.lucene.document.StringField
 import org.apache.lucene.document.TextField
-import org.apache.lucene.index.IndexWriter
+import org.apache.lucene.queryparser.classic.QueryParser
 import org.junit.{After, Before, Test}
-class QueryCase{
+import java.nio.file.Paths
+import org.apache.lucene.index.DirectoryReader
+import org.apache.lucene.search.IndexSearcher
+import org.apache.lucene.store.FSDirectory
+import org.apache.lucene.analysis.standard.StandardAnalyzer
 
-  var writer: IndexWriter = Index.createWriter("./data")
+class IndexSearchCase{
+
+  val indexDir = "./data"
 
   def createDocument(id: Int, firstName: String, lastName: String, website: String): Document =
   {
@@ -23,19 +29,37 @@ class QueryCase{
 
   @Before
   def buildIndex(): Unit ={
+    val writer = Index.createWriter(indexDir)
     writer.addDocument(createDocument(1, "blue", "joe", "talent.com"))
     writer.addDocument(createDocument(2, "excelwang", "wang", "test.com"))
     writer.addDocument(createDocument(3, "zhongxin", "liu", "angle.com"))
+    writer.close();
   }
 
   @After
   def close(): Unit ={
+    val writer = Index.createWriter(indexDir)
     writer.deleteAll();
     writer.close();
   }
 
   @Test
   def count(): Unit ={
-    println(writer.numRamDocs())
+    val dir = FSDirectory.open(Paths.get(indexDir))
+    val reader = DirectoryReader.open(dir)
+    assert(reader.numDocs()==3)
+  }
+
+  @Test
+  def search(): Unit ={
+    val firstName: String = "blue"
+    val dir = FSDirectory.open(Paths.get(indexDir))
+    val reader = DirectoryReader.open(dir)
+    val searcher = new IndexSearcher(reader)
+    val qp = new QueryParser("firstName", new StandardAnalyzer)
+    val nameQuery = qp.parse(firstName)
+    val hits = searcher.search(nameQuery, 10)
+    assert(hits.scoreDocs.length!=0)
+    hits.scoreDocs.map(d => println(searcher.doc(d.doc).get("firstName")))
   }
 }
