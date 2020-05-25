b0b196f8ffbc6bbe3761a3789c3d19eb53e4c5c9
excelwang <wanghuajin999@163.com>
2020-05-25 15:15:06 +0800

    config bug fix

--- src/main/scala/cn/pandadb/costore/config/Config.scala
+++ src/main/scala/cn/pandadb/costore/config/Config.scala
@@ -7,7 +7,7 @@ class Config(val nodesInfo: List[String], val replicaFactor: Int) {
   val vNodeRing = new ConsistentHashRing(vNodeIDs.map(id => id.toString))
   def route(node: Map[String, String]): List[(Int, String)] = {
     val primaryVNodeID = vNodeRing.getHolder(node.get("id").get, replicaFactor).toInt
-    val beginIndex = nodesInfo.indexOf(vNodeID2NodeInfo.get(primaryVNodeID).get)
-    (0 until replicaFactor).toList.map(inc => (primaryVNodeID, nodesInfo(beginIndex+inc)))
+    val beginIndex = nodesInfo.indexOf(vNodeID2NodeInfo.get(primaryVNodeID).get)//TODO improve perf.
+    (0 until replicaFactor).toList.map(inc => (primaryVNodeID, nodesInfo((beginIndex+inc)%nodesInfo.length)))
   }
 }
