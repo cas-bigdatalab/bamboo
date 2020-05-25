39a026084d1559626acac367edf1ca5a54dabd9a
excelwang <wanghuajin999@163.com>
2020-05-25 17:09:46 +0800

    add replica VNode to following nodes

--- src/main/scala/cn/pandadb/costore/config/Config.scala
+++ src/main/scala/cn/pandadb/costore/config/Config.scala
@@ -5,14 +5,12 @@ class Config(val nodesInfo: List[String], val replicaFactor: Int) {
   val vNodeIDs = 0 until nodesInfo.length * globalConfig.vNodeNumberPerNode toList
   val vNodeID2NodeInfo = vNodeIDs.map(vid => (vid -> nodesInfo(vid % nodesInfo.length))).toMap
   val vNodeRing = new ConsistentHashRing(vNodeIDs.map(id => id.toString))
+  def getReplicaLocation(originLocation: String) = (0 until replicaFactor).toList.map(inc => {
+    val beginIndex = nodesInfo.indexOf(originLocation)//TODO improve perf.
+    nodesInfo((beginIndex+inc)%nodesInfo.length)
+  })
   def route(node: Map[String, String]): List[(Int, String)] = {
     val primaryVNodeID = vNodeRing.getHolder(node.get("id").get, replicaFactor).toInt
-    val beginIndex = nodesInfo.indexOf(vNodeID2NodeInfo.get(primaryVNodeID).get)//TODO improve perf.
-    println(beginIndex)
-    (0 until replicaFactor).toList.map(inc => {
-      val cur = (beginIndex+inc)%nodesInfo.length
-      println(cur)
-      (primaryVNodeID, nodesInfo(cur))
-    })
+    getReplicaLocation(vNodeID2NodeInfo.get(primaryVNodeID).get).map(l=>(primaryVNodeID, l))
   }
 }
