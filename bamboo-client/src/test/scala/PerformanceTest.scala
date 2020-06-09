918d9808025e6fa58b8080dfb001f895e6aacae2
excelwang <wanghuajin999@163.com>
2020-06-09 17:52:20 +0800

    opt test

--- bamboo-client/src/test/scala/PerformanceTest.scala
+++ bamboo-client/src/test/scala/PerformanceTest.scala
@@ -1,39 +1,65 @@
 
 import cn.pandadb.bamboo.Client
-import org.junit.{Before, Test}
+import org.junit.Test
 
 class PerformanceTest{
 
-  val client = new Client(List("localhost:11234"))
-
-  // scalastyle:off
-  @Before
-  def buildIndex(): Unit = {
-    val  iters = 1024
-    val start = System.currentTimeMillis
-    (1 to iters).par.foreach(id => {
-      client.addNode(Map("id" -> s"$id", "name" -> s"bluejoe_$id", "url" -> s"talent.com_$id"))
-    })
-    val end = System.currentTimeMillis
-
-    println(s"write $iters nodes to bamboo cost " + (end-start) + " ms")
-  }
-
-//  @After
-//  def clearIndex(): Unit ={
-//    client.deleteAll()
-//  }
+  val rClient = new Client(List("10.0.82.216:11234", "10.0.82.217:11234", "10.0.82.218:11234"), balancePolicy = "RND", balancePolicyBatch = 30)
+  val lClient = new Client(List("localhost:11234", "localhost:11235", "localhost:11236"), balancePolicy = "RND", balancePolicyBatch = 30)
 
   @Test
-  def search(): Unit = {
-    Thread.sleep(20000)
-    val ret = client.filterNodes(Map(("name" -> "bluejoe_1024")))
-    println(ret)
+  def searchIndex(): Unit = {
+    val batchCNT = 200
+    val batchSize = 3
+    Tool.searchIndex(lClient.filterNodes, batchCNT*batchSize)
+  }
+
+  @Test
+  def buildIndexSyn(): Unit = {
+    val batchCNT = 200
+    val batchSize = 3
+    lClient.deleteAll()
+    Tool.buildIndex(lClient.addNode, batchCNT, batchSize)
+  }
+
+  @Test
+  def buildIndexAsyn(): Unit = {
+    val batchCNT = 200
+    val batchSize = 3
+    lClient.deleteAll()
+    Tool.buildIndex(lClient.addNodeAsyn, batchCNT, batchSize)
   }
 
   @Test
   def deleteIndex(): Unit = {
-    client.deleteAll()
+    lClient.deleteAll()
   }
-  // scalastyle:on
+
+  @Test
+  def remoteSearchIndex(): Unit = {
+    val batchCNT = 200
+    val batchSize = 3
+    Tool.searchIndex(rClient.filterNodes, batchCNT*batchSize)
+  }
+
+  @Test
+  def remoteBuildIndexSyn(): Unit = {
+    val batchCNT = 200
+    val batchSize = 3
+    rClient.deleteAll()
+    Tool.buildIndex(rClient.addNode, batchCNT, batchSize)
+  }
+
+  @Test
+  def remoteBuildIndexAsyn(): Unit = {
+    val batchCNT = 200
+    val batchSize = 3
+    rClient.deleteAll()
+    Tool.buildIndex(rClient.addNodeAsyn, batchCNT, batchSize)
+  }
+
+  @Test
+  def remoteDeleteIndex(): Unit = {
+    rClient.deleteAll()
+  }
 }
---
+++ bamboo-client/src/test/scala/Tool.scala
@@ -0,0 +1,30 @@
+object Tool {
+  // scalastyle:off
+  def buildIndex(indexF: Map[String, String] => Any, batchCNT: Int, batchSize: Int, warmingCNT: Int = 10): Unit = {
+    var start: Long = 0
+    (1  to batchCNT + warmingCNT).foreach(oid => {
+      if (oid == warmingCNT + 1) {//skip warmer iters
+        start = System.currentTimeMillis
+      }
+      (1  to batchSize).par.foreach(id => {
+        val pid = id + batchSize*(oid - 1)
+        val doc = Map("id" -> s"$pid", "name" -> s"bluejoe_$pid", "url" -> s"talent.com_$pid")
+        indexF(doc)
+      })
+    })
+    val end = System.currentTimeMillis
+    println(s"write ${batchCNT * batchSize / (end - start).toFloat * 1000} nodes per second to bamboo")
+  }
+
+  def searchIndex(searchF: Map[String, String] => List[Map[String, String]], totalCNT: Int): Unit = {
+    val interval = totalCNT/10
+    (1 to 10).foreach(oid => {
+      val start = System.currentTimeMillis
+      val q = Map("name" -> s"bluejoe_${oid*interval}")
+      val ret = searchF(q)
+      val end = System.currentTimeMillis
+      println(s"search: $q, return: $ret, cost ${end - start} ms")
+    })
+  }
+
+}
